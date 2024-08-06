4f06be98f31335bd05ce7daed92bfee166412c75
hj <15289450519@qq.com>
2024-08-06 14:08:07 +0800

    添加base路径

---
+++ assets/zh_foo.md.m_6vwc3Z.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./chunks/framework.DHTkf6oL.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"zh/foo.md","filePath":"zh/foo.md"}'),a={name:"zh/foo.md"};function r(s,c,n,f,p,_){return t(),o("div")}const m=e(a,[["render",r]]);export{i as __pageData,m as default};
